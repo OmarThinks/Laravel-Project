8a80f5fb2ac8d882390389a551b526ec522b84a1
Omar Magdy <contact.parmag@gmail.com>
2021-06-07 22:03:35 +0200

    testCase edited

--- application/tests/Feature/ExampleTest.php
+++ application/tests/Feature/ExampleTest.php
@@ -5,17 +5,30 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+
+
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
     public function test_example()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
+
+    public function test_example2()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+
 }
+
+
+
+
+
+?>
