d1ba4f957be9a3ffcd2037eb4d43bce4cd5b80da
Omar Magdy <contact.parmag@gmail.com>
2021-09-17 00:00:28 +0200

    welcome:renamed:home

---
+++ my_application/resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products Application</title>
+</head>
+<body>
+
+
+<div>
+    <a href="/hi/">Hi</a>
+</div>
+
+
+<div>
+    <a href="/hi/json/">Hi JSON</a>
+</div>
+
+
+<div>
+    <a href="/products/">Products List</a>
+</div>
+
+
+
+
+</body>
+</html>
+
