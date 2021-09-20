21035113f0c163a1bd7fe2cf2b87cdea6f48ed2a
Omar Magdy <contact.parmag@gmail.com>
2021-09-20 16:36:28 +0200

    all product details

--- my_application/resources/views/products_details.blade.php
+++ my_application/resources/views/products_details.blade.php
@@ -2,7 +2,49 @@
 
 @section("content")
 
-Product Id is:
+<h1>
+{{$title}}
+</h1>
+
+<div>
+
+<p>
+
+Id:
 {{$product->id}}
+</p>
+
+
+
+<p>
+
+Name:
+{{$product->name}}
+</p>
+
+
+
+<p>
+
+Description:
+{{$product->description}}
+</p>
+
+
+
+<p>
+
+Price:
+{{$product->price}}
+</p>
+
+
+
+
+
+
+
+</div>
+
 
 @endsection
