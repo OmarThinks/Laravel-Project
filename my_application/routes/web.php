d4feeac4d56212d47b3f91a3e75d3a1e3f655046
Omar Magdy <contact.parmag@gmail.com>
2021-09-20 23:02:26 +0200

    used bootstrap instead of vue

--- my_application/routes/web.php
+++ my_application/routes/web.php
@@ -56,3 +56,7 @@ Route::delete('/products/{id}', [ProductsController::class, 'destroy']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
