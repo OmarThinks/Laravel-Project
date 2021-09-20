bf57dbd8ffdfb29301490e268f7c799c5654da87
Omar Magdy <contact.parmag@gmail.com>
2021-09-21 00:23:04 +0200

    on submit create product

--- my_application/resources/views/products/create.blade.php
+++ my_application/resources/views/products/create.blade.php
@@ -17,19 +17,20 @@
 
 <div>
 
-<form method="POST" action="/products">
+<form onsubmit="return createProduct(event)">
 	@csrf
 <div>
 	<label>Name:</label>
-	<input type="text" name="name">
+	<input type="text" name="name" id="name_input">
 </div>
 <div>
 	<label>Description:</label>
-	<textarea name="description"></textarea>
+	<textarea name="description" id="description_input"></textarea>
 </div>
 <div>
 	<label>Price:</label>
-	<input type="number" name="price" step="0.01"></input>
+	<input type="number" id="price_input" 
+	name="price" step="0.01"></input>
 </div>
 
 
@@ -38,9 +39,21 @@
 
 
 </form>
-	
+
 
 </div>
+
+
+
+<script type="text/javascript">
+function createProduct(event)
+{
+	event.preventDefault();
+	console.log("I will create a product");
+}
+</script>
+
+
 
 
 @endsection
